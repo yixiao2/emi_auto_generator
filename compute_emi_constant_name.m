3811059f5186ed7b8541e81d885bb4c8446d085d
Gautam Bisht <gbisht@lbl.gov>
2018-01-25 15:31:07 -0800

    Adds description about the code

--- compute_emi_constant_name.m
+++ compute_emi_constant_name.m
@@ -1,3 +1,22 @@
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+% 
+% Given a variables, computes a the EMI contants variable name.
+%
+% EMI_VAR_CONTANT_NAME = CONSTANT_PREFIX '_' CONSTANT_NAME
+%
+% where 
+%   CONSTANT_PREFIX = var.emi_constant_prefix    if var.emi_constant_prefix exists
+%                   = constant_prefix
+%
+%   CONSTANT_NAME   = var.emi_constant_name      if var.emi_constant_name exists
+%                   = upper(var.alm_name)
+%
+% E.g. of EMI contant variable name are:
+%  - 'L2E_STATE_FORC_PBOT_DOWNSCALED'
+%  - 'E2L_STATE_FSUN'
+%  - 'L2E_PARAMETER_WATSATC'
+%
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 function emi_constant_name = compute_emi_constant_name(var,constant_prefix)
 
 if (~isfield(var,'emi_constant_name'))
