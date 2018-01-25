63ff066ea041397acb38843dc29fa01a15834bb3
Gautam Bisht <gbisht@lbl.gov>
2018-01-25 13:58:30 -0800

    adds driver code

---
+++ emi_derivetype_auto_generator.m
@@ -0,0 +1,12 @@
+function emi_derivetype_auto_generator()
+
+Atm2LndType();
+CanopyStateType();
+ChemStateType();
+ColumnType();
+EnergyFluxType();
+SoilHydrologyType();
+SoilStateType();
+TemperatureType();
+WaterFluxType();
+WaterStateType();
