06538510321a14faaa74b03275a17951118a2712
Gautam Bisht <gbisht@lbl.gov>
2018-01-25 15:21:05 -0800

    Adds description about the code

--- compute_emi_constant_names.m
+++ compute_emi_constant_names.m
@@ -1,3 +1,16 @@
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+% 
+% Given a list of variables, computes a list of EMI contants variable
+% names.
+% 
+% E.g. of contant_prefix = 'L2E_STATE'
+% 
+% E.g. of EMI contant variable names
+%  - 'L2E_STATE_FORC_PBOT_DOWNSCALED'
+%  - 'E2L_STATE_FSUN'
+%  - 'L2E_PARAMETER_WATSATC'
+%
+% +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 function emi_constant_names = compute_emi_constant_names(vars,constant_prefix)
 
 emi_constant_names = '';
